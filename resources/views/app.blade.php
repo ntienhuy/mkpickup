7008d7d7e657abb490ba2efc01e08964efb9cd14
HuyNguyen <tienhuy1412@gmail.com>
2015-06-10 11:07:03 +0700

    update app.blade.php

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -37,47 +37,11 @@
             </div>
             <div class="navbar-collapse collapse" id="navbar-main">
                 <ul class="nav navbar-nav">
-                    <li class="dropdown">
-                        <a class="dropdown-toggle" data-toggle="dropdown" href="#" id="themes">Themes <span class="caret"></span></a>
-                        <ul class="dropdown-menu" aria-labelledby="themes">
-                            <li><a href="../default/">Default</a></li>
-                            <li class="divider"></li>
-                            <li><a href="../cerulean/">Cerulean</a></li>
-                            <li><a href="../cosmo/">Cosmo</a></li>
-                            <li><a href="../cyborg/">Cyborg</a></li>
-                            <li><a href="../darkly/">Darkly</a></li>
-                            <li><a href="../flatly/">Flatly</a></li>
-                            <li><a href="../journal/">Journal</a></li>
-                            <li><a href="../lumen/">Lumen</a></li>
-                            <li><a href="../paper/">Paper</a></li>
-                            <li><a href="../readable/">Readable</a></li>
-                            <li><a href="../sandstone/">Sandstone</a></li>
-                            <li><a href="../simplex/">Simplex</a></li>
-                            <li><a href="../slate/">Slate</a></li>
-                            <li><a href="../spacelab/">Spacelab</a></li>
-                            <li><a href="../superhero/">Superhero</a></li>
-                            <li><a href="../united/">United</a></li>
-                            <li><a href="../yeti/">Yeti</a></li>
-                        </ul>
+                    <li>
+                        <a href="{{action('WelcomeController@index')}}">Trang chủ</a>
                     </li>
                     <li>
-                        <a href="../help/">Help</a>
-                    </li>
-                    <li>
-                        <a href="#">Blog</a>
-                    </li>
-                    <li class="dropdown">
-                        <a class="dropdown-toggle" data-toggle="dropdown" href="#" id="download">Download <span class="caret"></span></a>
-                        <ul class="dropdown-menu" aria-labelledby="download">
-                            <li><a href="#">bootstrap.min.css</a></li>
-                            <li><a href="#">bootstrap.css</a></li>
-                            <li class="divider"></li>
-                            <li><a href="#">variables.less</a></li>
-                            <li><a href="#">bootswatch.less</a></li>
-                            <li class="divider"></li>
-                            <li><a href="#">_variables.scss</a></li>
-                            <li><a href="#">_bootswatch.scss</a></li>
-                        </ul>
+                        <a href="{{action('ShowRouteController@index')}}">Đặt xe</a>
                     </li>
                 </ul>
 
